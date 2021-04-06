bb0874e1a9dcaf43e49859929eeb10295a6ea3eb
root <root@DESKTOP-S049SIQ.localdomain>
2021-04-06 15:47:33 +0800

    update contracts

--- contracts/proposer.js
+++ contracts/proposer.js
@@ -19,9 +19,10 @@ var validatorsInstance = new web3.eth.Contract(validators.abi, validatorAddress)
 const punish = JSON.parse(fs.readFileSync("./build/contracts/Punish.json"));
 var punishInstance = new web3.eth.Contract(punish.abi, punishAddress);
 
+const address = ""
 //创建验证人提议
-proposalInstance.methods.createProposal("被提议人地址", "")
-    .send({ from: '提议人地址' }).
+proposalInstance.methods.createProposal(address, "")
+    .send({ from: address }).
     then(result => {
         console.log(result)
         var id = result.events.LogCreateProposal.returnValues.id
@@ -31,13 +32,13 @@ proposalInstance.methods.createProposal("被提议人地址", "")
     })
 
 //通过半数以上投票后，创建为验证人
-validatorsInstance.methods.createOrEditValidator("待创建验证人地址","","","","","").send({from:'待创建验证人地址'}).then(instance => {
-    console.log(instance)
-    validatorsInstance.methods.getActiveValidators().call().then(console.log);
-})
+// validatorsInstance.methods.createOrEditValidator(address,"","","","","").send({from:address}).then(instance => {
+//     console.log(instance)
+//     validatorsInstance.methods.getActiveValidators().call().then(console.log);
+// })
 
 //向验证人至少质押32个以太坊单位货币才能成为活跃验证人，活跃验证人在一个区块epoch后更新
-validatorsInstance.methods.stake("0xA197CE6cd9901219f128e4D27d448dF55a60ECeF").send({ from: '0x2334a96f67B6C8906954a7b56333271e142dB36d', value: helpers.ether("32") }).then(console.log);
+// validatorsInstance.methods.stake(address).send({ from: address, value: helpers.ether("32") }).then(console.log);
 
 
 
--- contracts/validator.js
+++ contracts/validator.js
@@ -19,17 +19,18 @@ var validatorsInstance = new web3.eth.Contract(validators.abi, validatorAddress)
 const punish = JSON.parse(fs.readFileSync("./build/contracts/Punish.json"));
 var punishInstance = new web3.eth.Contract(punish.abi, punishAddress);
 
+const address = ""
 //对提议投票，赞成true, 反对false
-proposalInstance.methods.voteProposal("提议id", true).send({ from: '审批人地址' }).then(instance => {
-    console.log(instance)
-})
+// proposalInstance.methods.voteProposal(address, true).send({ from: address }).then(instance => {
+//     console.log(instance)
+// })
 
 //向验证人质押32个以太坊货币单位
-validatorsInstance.methods.stake("质押地址").send({ from: '质押人地址', value: helpers.ether("32") }).then(instance => {
+validatorsInstance.methods.stake(address).send({ from: address, value: helpers.ether("32") }).then(instance => {
     console.log(instance)
     //查看质押信息
-    validatorsInstance.methods.getValidatorInfo("质押地址").call().then(console.log);
+    validatorsInstance.methods.getValidatorInfo(address).call().then(console.log);
 });
 
 //取消质押
-validatorsInstance.methods.unstake("质押地址").send({ from: '质押人地址'}).then(console.log);
+validatorsInstance.methods.unstake(address).send({ from: address}).then(console.log);
