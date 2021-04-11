22ac4c6f8c389843347c26deff840e15dee28eff
gogolxdong <lxdong.007@gmail.com>
2021-04-11 09:27:45 +0800

    save local

--- start.sh
+++ start.sh
@@ -4,12 +4,10 @@
 --port 30303 \
 --mine \
 --nousb \
---unlock 0x2cba6f401df45b113b073f8609985ac47186854d \
+--unlock 0xcd0c087aa41fb181bf78d904a0be1af6da42dbff \
 --password password.txt \
 --miner.threads=1 \
 --allow-insecure-unlock \
---bootnodes "enode://bde9c993f41e2a180d48b7551642bfe76465eb132514a2fa9adcd10163b1faca64377d448ddeac0e4fed37c3fd34755a16fa9c38b4ea865e6b57dc4207c971d6@103.215.3.169:30303" \
---syncmode "full" \
 --http \
 --http.port 8545 \
 --http.addr 0.0.0.0 \
